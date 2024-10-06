53f59a92636c5b6e1c43c289fb22565b0e2c85ef
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2024-10-06 11:03:12 +0530

    added mysql automation file

---
+++ mysql.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+LOGS_FOLDER="/var/log/Expense/" #folder path Expense created  in  /var/log
+
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #created scriptname with removing .sh here $0 redirctors which will give script name.
+
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S) # creating time stamp
+
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log" #here creating strutured logfile
+
+mkdir -p $LOGS_FOLDER #we dont have shell-script folder so for that we are creating
+
+USER_ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT() {
+    if [ $USER_ID -ne 0 ]; then
+        # here tee command will append the result in logfile before | (pipe) it will print on the terminal also
+        #if your using tee command the no need of &>>
+        echo -e "$R please run the script with root privilages $N" | tee -a $LOG_FILE 
+        exit 1
+    fi
+}
+
+validate() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 is $R not success.. Check it.$N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is $G Sucessfull...$N"  | tee -a $LOG_FILE
+    fi
+}
+
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+CHECK_ROOT
+
+dnf install mysql-server -y
+validate $? "Installing mysql server.."
+
+systemctl enable mysqld
+validate $? "enables mysql server.."
+
+systemctl start mysqld
+validate $? "started mysql server.."
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+validate $? "setting up root password.."
